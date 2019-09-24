77ccc77e5ae8c45cae36b2f72a7a15aaf3adb127
caviarLuv <yashleycaviar@gmail.com>
2019-09-23 19:08:21 -0700

    added static files and DB manipulation

--- team9/collegeWebPortal/models.py
+++ team9/collegeWebPortal/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+	student_id = models.IntegerField(max_length=9)
+	first_name = models.CharField(max_length=30)
+	last_name = models.CharField(max_length=30)
+
+	def getFUllName(self):
+		return '%s %s' % (self.first_name, self.last_name)
+		
