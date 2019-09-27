b29d14b42aa9784a83d704987cbb20ab64f47a15
Steven Myers <myers9s123@gmail.com>
2019-09-27 16:12:28 -0700

    Install django-crispy-forms and set template pack to use Bootstrap 4.

--- team9/team9/settings.py
+++ team9/team9/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third party dependencies
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Django crispy forms template
+# https://django-crispy-forms.readthedocs.io/en/latest/install.html#template-packs
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
