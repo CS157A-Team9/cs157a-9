ae6017d6f3a542ecdb802c5a77bfcea5c30af3c4
Steven Myers <myers9s123@gmail.com>
2019-10-01 18:06:40 -0700

    First working version of authentication flow
    Add dashboard base template to be used for student portal.
    Refactor template content blocks.
    Install open-ionic icons.

--- team9/collegeWebPortal/views.py
+++ team9/collegeWebPortal/views.py
@@ -1,7 +1,7 @@
-#from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-# 
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def index(request):
-	return HttpResponse("Testing! :)");
+	return render(request, 'collegeWebPortal/index.html')
--- team9/team9/settings.py
+++ team9/team9/settings.py
@@ -14,8 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -41,6 +39,7 @@ INSTALLED_APPS = [
     # Third party dependencies
     'crispy_forms',
     # Internal apps
+    'team9',
     'collegeWebPortal',
 ]
 
@@ -130,6 +129,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+
+# Login redirect configuration
+LOGIN_URL = '/accounts/login' # Default
+LOGIN_REDIRECT_URL = '/'
 
 
 # Django crispy forms template
