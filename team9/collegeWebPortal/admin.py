289f6a8c20c442f9e425046d6eba13e3e2ac8a62
Steven Myers <myers9s123@gmail.com>
2019-10-11 18:23:16 -0700

    Define models for student, professor and department

--- team9/collegeWebPortal/admin.py
+++ team9/collegeWebPortal/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Student
+from .models import Department, Professor, Student
 
+admin.site.register(Department)
+admin.site.register(Professor)
 admin.site.register(Student)
---
+++ team9/collegeWebPortal/models/profile_base.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+"""
+I have chosen explicitly to use a one-to-one field to the
+default User model instead of using multi-table inheritance
+due to some known issues with the latter
+"""
+class ProfileBase(models.Model):
+    user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
