decdbf570237615c88235fe3f5445224f06d5330
Steven Myers <myers9s123@gmail.com>
2019-10-30 20:48:39 -0700

    Fix signal which auto creates Student/Professor data.

--- team9/collegeWebPortal/apps.py
+++ team9/collegeWebPortal/apps.py
@@ -1,12 +1,8 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_save
 
 
 class CollegewebportalConfig(AppConfig):
     name = 'collegeWebPortal'
 
     def ready(self):
-        from django.contrib.auth.models import User
         from .signals import ensure_profile_exists
-
-        post_save.connect(ensure_profile_exists, sender=User, dispatch_uid='ensure_profile_exists')
