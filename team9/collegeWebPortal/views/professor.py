a876b8cee8837416949c42cc3c26ad914b2eb954
caviarLuv <yashleycaviar@gmail.com>
2019-11-06 01:37:56 -0800

    profess profile view is able to handle updates and show up data from the database

--- team9/collegeWebPortal/views/professor.py
+++ team9/collegeWebPortal/views/professor.py
@@ -1,8 +1,9 @@
 from django.conf import settings
+from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from collegeWebPortal.decorators import group_required
-
+from collegeWebPortal.models import Professor
 
 @login_required
 @group_required(settings.GROUP_PROFESSORS)
@@ -17,4 +18,61 @@ def courses(request):
 @login_required
 @group_required(settings.GROUP_PROFESSORS)
 def profile(request):
-    return render(request, 'collegeWebPortal/professor/profile.html')
+	professor = Professor.objects.get(pk=request.user.id)
+	return render(request, 'collegeWebPortal/professor/profile.html', {'usr': professor})
+
+@login_required
+@group_required(settings.GROUP_PROFESSORS)
+def edit_bio(request):
+	if request.method == 'POST':
+		bio = request.POST.get('bio', None)
+		try: 
+			professor = Professor.objects.get(pk=request.user.id)
+			professor.bio = bio
+			professor.save()
+			#return render(request, 'collegeWebPortal/professor/profile.html', {'usr': professor})
+			return redirect('/professor/profile')
+		except Professor.DoesNotExist:
+			return HttpResponse("error")
+	else:
+		professor = Professor.objects.get(pk=request.user.id)
+		showbioform = "True"
+		return render(request, 'collegeWebPortal/professor/profile.html', {'usr': professor, 'showbioform': showbioform})
+
+
+@login_required
+@group_required(settings.GROUP_PROFESSORS)
+def edit_contact(request):
+	if request.method == 'POST':
+		contact = request.POST.get('contact', None)
+		try: 
+			professor = Professor.objects.get(pk=request.user.id)
+			professor.contact_info = contact
+			professor.save()
+			#return render(request, 'collegeWebPortal/professor/profile.html', {'usr': professor})
+			return redirect('/professor/profile')
+		except Professor.DoesNotExist:
+			return HttpResponse("error")
+	else:
+		professor = Professor.objects.get(pk=request.user.id)
+		showcontactform = "True"
+		return render(request, 'collegeWebPortal/professor/profile.html', {'usr': professor, 'showcontactform': showcontactform})
+
+
+@login_required
+@group_required(settings.GROUP_PROFESSORS)
+def edit_hour(request):
+	if request.method == 'POST':
+		hour = request.POST.get('hour', None)
+		try: 
+			professor = Professor.objects.get(pk=request.user.id)
+			professor.office_hours = hour
+			professor.save()
+			#return render(request, 'collegeWebPortal/professor/profile.html', {'usr': professor})
+			return redirect('/professor/profile')
+		except Professor.DoesNotExist:
+			return HttpResponse("error")
+	else:
+		professor = Professor.objects.get(pk=request.user.id)
+		showhourform = "True"
+		return render(request, 'collegeWebPortal/professor/profile.html', {'usr': professor, 'showhourform': showhourform})
